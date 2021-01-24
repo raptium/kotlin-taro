01adcce98b7aec450929de2c7ae8d5b4c837fb8f
Guan Hao <raptium@gmail.com>
2021-01-24 17:00:04 +0800

    remove unnecessary npm dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,10 +22,4 @@ dependencies {
     api(kotlin("stdlib-js"))
     api("org.jetbrains:kotlin-react:${version("kotlin-react")}")
     api("org.jetbrains:kotlin-css:${version("kotlin-css")}")
-
-    compileOnly(npm("@tarojs/taro", version("taro")))
-    compileOnly(npm("@tarojs/components", version("taro")))
-    compileOnly(npm("@tarojs/runtime", version("taro")))
-    compileOnly(npm("@tarojs/react", version("taro")))
-    compileOnly(npmv("react"))
 }
